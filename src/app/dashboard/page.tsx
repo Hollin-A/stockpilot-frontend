9ca59cc920629a566efa75562481c21cb9ac34d0
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-16 01:20:57 +1100

    feat: add shadcn UI components

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@
 import DashboardLayout from "@/components/layout/dashboard-layout";
 import { useEffect, useState } from "react";
 import { api } from "@/lib/api";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function DashboardPage() {
   const [sales, setSales] = useState<any>(null);
@@ -23,6 +24,14 @@ export default function DashboardPage() {
           <p>Total Revenue: {sales.totalRevenue}</p>
         </div>
       )}
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Total Revenue</CardTitle>
+        </CardHeader>
+
+        <CardContent>$500</CardContent>
+      </Card>
     </DashboardLayout>
   );
 }
