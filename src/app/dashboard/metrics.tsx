3c96567fa7cf3a68b58a049ac7919b96fad440d1
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-16 01:28:26 +1100

    feat: implement dashboard metric cards

---
+++ src/app/dashboard/metrics.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function Metrics({ sales }: any) {
+  return (
+    <div className="grid grid-cols-4 gap-6 mb-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Total Revenue</CardTitle>
+        </CardHeader>
+        <CardContent className="text-2xl font-bold">
+          ${sales?.totalRevenue || 0}
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Total Orders</CardTitle>
+        </CardHeader>
+        <CardContent className="text-2xl font-bold">
+          {sales?.totalOrders || 0}
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Products</CardTitle>
+        </CardHeader>
+        <CardContent className="text-2xl font-bold">--</CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Low Stock</CardTitle>
+        </CardHeader>
+        <CardContent className="text-2xl font-bold text-red-500">
+          --
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import DashboardLayout from "@/components/layout/dashboard-layout";
 import { useEffect, useState } from "react";
 import { api } from "@/lib/api";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Metrics from "./metrics";
 
 export default function DashboardPage() {
   const [sales, setSales] = useState<any>(null);
@@ -18,20 +19,7 @@ export default function DashboardPage() {
     <DashboardLayout>
       <h1 className="text-2xl font-bold text-slate-700">Dashboard</h1>
 
-      {sales && (
-        <div>
-          <p>Total Orders: {sales.totalOrders}</p>
-          <p>Total Revenue: {sales.totalRevenue}</p>
-        </div>
-      )}
-
-      <Card>
-        <CardHeader>
-          <CardTitle>Total Revenue</CardTitle>
-        </CardHeader>
-
-        <CardContent>$500</CardContent>
-      </Card>
+      <Metrics sales={sales} />
     </DashboardLayout>
   );
 }
