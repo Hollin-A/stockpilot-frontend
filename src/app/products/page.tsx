18a5cc00011cc177d48042dc3527313e9b57762f
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2026-03-20 00:41:38 +1100

    feat: create products page route

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import DashboardLayout from "@/components/layout/dashboard-layout";
+
+export default function ProductsPage() {
+  return (
+    <DashboardLayout>
+      <h1 className="text-2xl font-bold">Products</h1>
+    </DashboardLayout>
+  );
+}
